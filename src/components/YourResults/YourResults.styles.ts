892ea3fce5451fd209325e1c2f6bed0645a2835e
haipinggong <ghaiping@hotmail.com>
2025-06-10 19:29:18 +1000

    Adjust the YourResults component styling

--- src/components/YourResults/YourResults.styles.ts
+++ src/components/YourResults/YourResults.styles.ts
@@ -1,21 +1,37 @@
 import type { SxProps, Theme } from "@mui/material";
 
 export const styles: Record<string, SxProps<Theme>> = {
-  container: {
+  container: (theme) => ({
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
     gap: 6,
+    [theme.breakpoints.up("sm")]: {
+      paddingInline: 5,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      gap: 9,
+    },
     // padding: "0 1rem",
-  },
-  image: {
+  }),
+  image: (theme) => ({
     width: "100%",
     borderRadius: 4,
-  },
-  resultContainer: {
+    [theme.breakpoints.up("sm")]: {
+      alignSelf: "flex-end",
+      maxWidth: "50%",
+    },
+  }),
+  resultContainer: (theme) => ({
     display: "flex",
     flexDirection: "column",
     gap: 4,
     paddingInline: 3,
-  },
+    [theme.breakpoints.up("sm")]: {
+      paddingInline: 0,
+      flex: 1,
+      paddingBlock: 5,
+      alignSelf: "center",
+    },
+  }),
 };
