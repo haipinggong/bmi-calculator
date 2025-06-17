e6284965281a3c1fa4a818a91dfaf19d00f06512
haipinggong <ghaiping@hotmail.com>
2025-06-17 19:11:54 +1000

    Add main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import { Hero } from "./components/Hero/Hero";
 import { YourResults } from "./components/YourResults/YourResults";
 import { Tips } from "./components/Tips/Tips";
@@ -5,12 +6,12 @@ import { Limits } from "./components/Limits/Limits";
 
 function App() {
   return (
-    <>
+    <Box component="main">
       <Hero />
       <YourResults />
       <Tips />
       <Limits />
-    </>
+    </Box>
   );
 }
 
